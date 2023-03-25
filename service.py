aaa7cf722e9bc9024e69713b6b4a5539178a9eed
Lordir <09mmnn@gmail.com>
2023-03-25 16:23:43 +0700

    Added posts v0

--- service.py
+++ service.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
+    print(url_for("main_page"))
     return render_template("main_page.html", title="Main")
+
+
+@app.route('/posts/<int:id>')
+def posts(id):
+    return f"Пользователь: {id}"
+    # return render_template("main_page.html", title="Profile")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Искусственный контекст
+# with app.test_request_context():
+#     print(url_for("main_page"))
+#     print(url_for("posts", id="1"))
