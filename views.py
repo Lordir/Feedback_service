2c2b3d11dd7f71249754d40e59c6c70c4bb700a3
Lordir <09mmnn@gmail.com>
2023-06-27 14:58:57 +0700

    Added views and templates for profile and reviews

--- views.py
+++ views.py
@@ -5,11 +5,21 @@ from models import *
 
 @app.route('/')
 def main_page():
-    print(url_for("main_page"))
-    return render_template("main_page.html", title="Main")
+    # print(url_for("main_page"))
+    return render_template("main_page.html", title="Главная страница")
 
 
-@app.route('/posts/<int:id>')
-def posts(id):
-    return f"Пользователь: {id}"
+@app.route('/profile/')
+def profile_page():
+    return render_template("profile.html", title="Профиль", name="name")
+
+
+@app.route('/reviews/')
+def reviews_page():
+    return render_template("reviews.html", title="Отзывы")
+
+
+@app.route('/review/<int:id>/')
+def review(id):
+    return f"Отзыв ID: {id}"
     # return render_template("main_page.html", title="Profile")
