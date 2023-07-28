562a1c6da23490e53cd62d59b8905487153f7c62
Lordir <09mmnn@gmail.com>
2023-07-28 20:26:58 +0700

    Added api sorted

--- api_views.py
+++ api_views.py
@@ -1,4 +1,5 @@
 import json
+import operator
 
 from flask import render_template
 from flask_login import login_user, login_required, logout_user, current_user
@@ -15,6 +16,36 @@ def reviews_api():
     result = []
     for review in range(len(reviews)):
         result.append(reviews[review].serialize())
+    result = json.dumps(result, ensure_ascii=False)
+
+    return result
+
+
+@app.route('/api/reviews/descending_sort_rating/')
+@login_required
+def reviews_descending_sort_rating_api():
+    user = db.session.execute(db.select(Users).filter_by(id=current_user.id)).scalar_one()
+    reviews = user.reviews
+    sort_reviews = sorted(reviews, key=operator.attrgetter('rating'))
+
+    result = []
+    for review in range(len(sort_reviews)):
+        result.append(sort_reviews[review].serialize())
+    result = json.dumps(result, ensure_ascii=False)
+
+    return result
+
+
+@app.route('/api/reviews/ascending_sort_rating/')
+@login_required
+def reviews_ascending_sort_rating_api():
+    user = db.session.execute(db.select(Users).filter_by(id=current_user.id)).scalar_one()
+    reviews = user.reviews
+    sort_reviews = sorted(reviews, key=operator.attrgetter('rating'), reverse=True)
+
+    result = []
+    for review in range(len(sort_reviews)):
+        result.append(sort_reviews[review].serialize())
     result = json.dumps(result, ensure_ascii=False)
 
     return result
@@ -52,6 +83,50 @@ def category_api(id):
 
         result = json.dumps(result, ensure_ascii=False)
 
+        return result
+    except:
+        return render_template('404.html')
+
+
+@app.route('/api/category/<int:id>/descending_sort_rating/')
+@login_required
+def category_descending_sort_rating_api(id):
+    try:
+        select_category = db.session.execute(db.select(Category).filter_by(id=id)).scalar_one()
+        category_reviews = select_category.review_id
+        category_reviews_at_current_user = []
+        for element in category_reviews:
+            if element.user_id == current_user.id:
+                category_reviews_at_current_user.append(element)
+        sort_reviews = sorted(category_reviews_at_current_user, key=operator.attrgetter('rating'))
+
+        result = []
+        for review in range(len(sort_reviews)):
+            result.append(sort_reviews[review].serialize())
+
+        result = json.dumps(result, ensure_ascii=False)
+        return result
+    except:
+        return render_template('404.html')
+
+
+@app.route('/api/category/<int:id>/ascending_sort_rating/')
+@login_required
+def category_ascending_sort_rating_api(id):
+    try:
+        select_category = db.session.execute(db.select(Category).filter_by(id=id)).scalar_one()
+        category_reviews = select_category.review_id
+        category_reviews_at_current_user = []
+        for element in category_reviews:
+            if element.user_id == current_user.id:
+                category_reviews_at_current_user.append(element)
+        sort_reviews = sorted(category_reviews_at_current_user, key=operator.attrgetter('rating'), reverse=True)
+
+        result = []
+        for review in range(len(sort_reviews)):
+            result.append(sort_reviews[review].serialize())
+
+        result = json.dumps(result, ensure_ascii=False)
         return result
     except:
         return render_template('404.html')
